daeb8d351b63827d1bbd7f163061539493bfeb2d
jarogross <127781032+jarogross@users.noreply.github.com>
2023-05-14 19:58:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function matematicke_operacie(x=4,y=2){
+console.log(x, "a", y)
+console.log("Vykonám matematické operácie")
+console.log(x,"+",y,"=", x+y)
+console.log(x,"-",y,"=", x-y)
+console.log(x,"/",y,"=", x/y)
+console.log(x,"*",y,"=", x*y)
+}
